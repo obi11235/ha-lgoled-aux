53a72f81140fc067b6a97664b8b94f14663392a8
paul <paul@frozenphoenix.net>
2023-01-07 13:34:53 -0500

    inital commit of service

---
+++ custom_components/lgoled_aux/__init__.py
@@ -0,0 +1,31 @@
+import asyncio
+from .aiopylgtv import WebOsClient
+
+
+DOMAIN = "lgoled_aux"
+
+ATTR_MODE = "mode"
+ATTR_IP = "ip"
+DEFAULT_MODE = "dolbyHdrStandard"
+
+
+def setup(hass, config):
+    """Set up is called when Home Assistant is loading our component."""
+
+    async def handle_picture_settings(call):
+        """Handle the service call."""
+        mode = call.data.get(ATTR_MODE, DEFAULT_MODE)
+        ip = call.data.get(ATTR_IP, None)
+
+        if ip == None:
+            return False
+
+        client = await WebOsClient.create(ip)
+        await client.disconnect()    
+        await client.connect()
+        await client.set_current_picture_mode(pic_mode=mode)
+
+    hass.services.register(DOMAIN, "set_picture_mode", handle_picture_settings)
+
+    # Return boolean to indicate that initialization was successful.
+    return True
